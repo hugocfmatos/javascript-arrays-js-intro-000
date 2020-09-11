9f27ea82f011f199827f3a5c472dee6819abde06
hugo matos <hugocfmatos@gmail.com>
2020-09-11 15:33:16 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -40,6 +40,6 @@ function destructivelyRemoveElementFromEndOfArray(array) {
 }
 
 fucntion removeElementFromEndOfArray(array) {
-  array=array.slice(0,array.length-1)
+  array.slice(0,array.length-1)
   return array
 }
