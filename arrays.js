4e8fc2dea1c8aa4240d3cfc1da3854f5801294be
hugo matos <hugocfmatos@gmail.com>
2020-09-11 15:15:47 +0000

    Automatically backed up by Learn

--- arrays.js
+++ arrays.js
@@ -23,3 +23,13 @@ function destructivelyAddElementToEndOfArray(array,element) {
 function accessElementInArray(array,index) {
   return array[index]
 }
+
+function destructivelyRemoveElementFromBeginningOfArray(array) {
+  array.shift()
+  return array
+}
+
+function removeElementFromBeginningOfArray(array) {
+  array=array.slice(1)
+  return array
+}
